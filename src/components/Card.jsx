3566b8224ca785dadf9ea9cc69093198278de655
nitatemic <alexandre@nitatemic.dev>
2023-02-07 18:09:27 +0100

    Main content layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card() {
+
+    return (
+        <div className="card">
+            <div className="card-content">
+                <div className="card-content-img">
+                    <img src="https://picsum.photos/340" alt="Image de la location"/>
+                </div>
+                <div className="card-content-container">
+                    <h3 className="card-content-container-title">Titre de la location</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,24 @@
 import TaglineBG from '../assets/img/TaglineBG.webp';
+import Card from "./Card.jsx";
 
 export default function Home() {
     return (
-        <div className="home-tagline">
-            <img className="home-tagline-img" src={TaglineBG} alt="Tagline Background"/>
-            <div className="home-tagline-title">
-                <h1 className="home-tagline-title-text">Chez vous, partout et ailleurs</h1>
+        <div className="home">
+            <div className="home-tagline">
+                <img className="home-tagline-img" src={TaglineBG} alt="Tagline Background"/>
+                <div className="home-tagline-title">
+                    <h1 className="home-tagline-title-text">Chez vous, partout et ailleurs</h1>
+                </div>
             </div>
+            <main className="home-main">
+                <div className="home-main-content">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+
+                </div>
+            </main>
         </div>
     )
 }
