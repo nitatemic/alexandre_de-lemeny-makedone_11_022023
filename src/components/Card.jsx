11f1ef34785811399b15bcbefe4dee6348b1a738
nitatemic <alexandre@nitatemic.dev>
2023-02-08 11:38:17 +0100

    Border on img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
         <div className="card">
             <div className="card-content">
                 <div className="card-content-img">
-                    <img src="https://picsum.photos/340" alt="Image de la location"/>
+                    <img src="https://picsum.photos/340" alt="Image de la location" className="card-content-img-file"/>
                 </div>
                 <div className="card-content-container">
                     <h3 className="card-content-container-title">Titre de la location</h3>
